d5e357ee58b4bcf8bfa87a69fc01045263407fdd
Yeo <jhoon2002@naver.com>
2022-10-25 23:18:09 +0900

    notebook

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,23 +10,13 @@ import { MysqlConfigModule } from '@/mysql-config/mysql-config.module'
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-            envFilePath: process.env.NODE_ENV === 'dev' ? '../.development.env' : '../.development.env',
+            envFilePath: process.env.NODE_ENV === 'dev' ? '../.development.env' : '../.productionY.env',
         }),
         TypeOrmModule.forRootAsync({
             imports: [MysqlConfigModule],
             useClass: MysqlConfigService,
             inject: [MysqlConfigService],
         }),
-        // TypeOrmModule.forRoot({
-        //     type: 'mysql',
-        //     host: 'localhost',
-        //     port: 3306,
-        //     username: 'apply_drama',
-        //     password: '1111',
-        //     database: 'apply_drama',
-        //     // entities: [User, Product], // 사용할 entity의 클래스명을 넣어둔다.
-        //     synchronize: false, // false로 해두는 게 안전하다.
-        // }),
     ],
     controllers: [AppController],
     providers: [AppService],
